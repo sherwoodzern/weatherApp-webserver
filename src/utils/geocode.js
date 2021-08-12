55e0522a1f2b53eaa4e2f976d67c43fcc39c0b4c
Sherwood Zern <sherwood.zern@oracle.com>
2021-08-12 18:18:16 -0400

    added code to read from environment variables

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,8 @@
 const request = require('postman-request');
 
+const apiKey = process.env.MAPBOX_API_KEY;
 const geocode = (address, callback) => {
-    const geocode = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?access_token=<mapbox API Key>&limit=1'
+    const geocode = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?access_token=' + apiKey + '&limit=1'
 
     request({url: geocode, json: true}, (error, response) => {
         if (error) {
